cf25668540c682a353e650c32fdb57937e1dc33b
Julian Andres Ariza <arjulian003@gmail.com>
2024-01-31 16:25:44 -0500

    Jumbotron add

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,8 +3,10 @@ import '../styles/Jumbotron.css'
 export function Jumbotron () {
     return (
         <div className="Jumbotron">
-            <h3>Mabel Zoraida Ariza Perez</h3>
-            <span></span>
+            <div className="JmboTxt">
+                <h3>Mabel Zoraida Ariza Perez</h3>
+                <span>Psicologa</span>
+            </div>
         </div>
     )
 }
