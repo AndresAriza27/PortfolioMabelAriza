45fac56f230edcc6a753ce9a66bced8b600c6cbe
Julian Andres Ariza <arjulian003@gmail.com>
2024-01-31 15:29:24 -0500

    Estudios Complementarios

--- src/components/Studies.jsx
+++ src/components/Studies.jsx
@@ -24,11 +24,149 @@ export function Studies () {
                 </div>
                 <div className="estudio">
                     <div className="FechaLugar">
-                        <div className="fecha">20/03/2019</div>
+                        <div className="fecha">17/11/2006</div>
+                        <div className="lugar">Chía</div>
+                    </div>
+                    <div className="TituloInstitucion">
+                        <div className="Titulo">Secretariado Auxiliar Contable </div>
+                        <div className="Institucion">SENA</div>
+                    </div>
+                    <div className="btnDescarga">
+                        <a href="#">
+                            <img src={ iconDwl } alt="Download" />
+                        </a>
+                    </div>
+                </div>
+                <div className="estudio">
+                    <div className="FechaLugar">
+                        <div className="fecha">12/12/2003</div>
+                        <div className="lugar">Zipaquirá</div>
+                    </div>
+                    <div className="TituloInstitucion">
+                        <div className="Titulo">Bachiller Académico</div>
+                        <div className="Institucion">Universidad o institución</div>
+                    </div>
+                    <div className="btnDescarga">
+                        <a href="#">
+                            <img src={ iconDwl } alt="Download" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <h3>Estudios Complementarios</h3>
+            <div className="estudios">
+                <div className="estudio">
+                    <div className="FechaLugar">
+                        <div className="fecha">01/07/2005</div>
                         <div className="lugar">Bogotá</div>
                     </div>
                     <div className="TituloInstitucion">
-                        <div className="Titulo">Psicología</div>
+                        <div className="Titulo">Fundamentos ISO 9001</div>
+                        <div className="Institucion">Universidad Manuela Beltrán</div>
+                    </div>
+                    <div className="btnDescarga">
+                        <a href="#">
+                            <img src={ iconDwl } alt="Download" />
+                        </a>
+                    </div>
+                </div>
+                <div className="estudio">
+                    <div className="FechaLugar">
+                        <div className="fecha">29/10/2008</div>
+                        <div className="lugar">Bogotá</div>
+                    </div>
+                    <div className="TituloInstitucion">
+                        <div className="Titulo">Salud Ocupacional</div>
+                        <div className="Institucion">Universidad Manuela Beltrán</div>
+                    </div>
+                    <div className="btnDescarga">
+                        <a href="#">
+                            <img src={ iconDwl } alt="Download" />
+                        </a>
+                    </div>
+                </div>
+                <div className="estudio">
+                    <div className="FechaLugar">
+                        <div className="fecha">29/08/2010</div>
+                        <div className="lugar">Bogotá</div>
+                    </div>
+                    <div className="TituloInstitucion">
+                        <div className="Titulo">Office Basico: Word, Excel y PowerPoint</div>
+                        <div className="Institucion">Universidad Manuela Beltrán</div>
+                    </div>
+                    <div className="btnDescarga">
+                        <a href="#">
+                            <img src={ iconDwl } alt="Download" />
+                        </a>
+                    </div>
+                </div>
+                <div className="estudio">
+                    <div className="FechaLugar">
+                        <div className="fecha">17/07/2018</div>
+                        <div className="lugar">Bogotá</div>
+                    </div>
+                    <div className="TituloInstitucion">
+                        <div className="Titulo">Redacción y Ortografía</div>
+                        <div className="Institucion">Sena</div>
+                    </div>
+                    <div className="btnDescarga">
+                        <a href="#">
+                            <img src={ iconDwl } alt="Download" />
+                        </a>
+                    </div>
+                </div>
+                <div className="estudio">
+                    <div className="FechaLugar">
+                        <div className="fecha">20/05/2017</div>
+                        <div className="lugar">Bogotá</div>
+                    </div>
+                    <div className="TituloInstitucion">
+                        <div className="Titulo">Comunicación Asertiva y efectiva en equipos de Trabajo</div>
+                        <div className="Institucion">Sena</div>
+                    </div>
+                    <div className="btnDescarga">
+                        <a href="#">
+                            <img src={ iconDwl } alt="Download" />
+                        </a>
+                    </div>
+                </div>
+                <div className="estudio">
+                    <div className="FechaLugar">
+                        <div className="fecha">29/06/2009</div>
+                        <div className="lugar">Chía</div>
+                    </div>
+                    <div className="TituloInstitucion">
+                        <div className="Titulo">Sensibilidad y Creatividad en el Servicio</div>
+                        <div className="Institucion">Asociación de empresarios de Chía</div>
+                    </div>
+                    <div className="btnDescarga">
+                        <a href="#">
+                            <img src={ iconDwl } alt="Download" />
+                        </a>
+                    </div>
+                </div>
+                <div className="estudio">
+                    <div className="FechaLugar">
+                        <div className="fecha">15/05/2010</div>
+                        <div className="lugar">Bogotá</div>
+                    </div>
+                    <div className="TituloInstitucion">
+                        <div className="Titulo">Finanzas Personales</div>
+                        <div className="Institucion">Universidad Manuela Beltrán</div>
+                    </div>
+                    <div className="btnDescarga">
+                        <a href="#">
+                            <img src={ iconDwl } alt="Download" />
+                        </a>
+                    </div>
+                </div>
+                <div className="estudio">
+                    <div className="FechaLugar">
+                        <div className="fecha">08/06/2018</div>
+                        <div className="lugar">Bogotá</div>
+                    </div>
+                    <div className="TituloInstitucion">
+                        <div className="Titulo">Diplomado en Gestión de Talento Humano</div>
                         <div className="Institucion">Institución Universitaria Politécnico Grancolombiano</div>
                     </div>
                     <div className="btnDescarga">
@@ -39,12 +177,12 @@ export function Studies () {
                 </div>
                 <div className="estudio">
                     <div className="FechaLugar">
-                        <div className="fecha">20/03/2019</div>
-                        <div className="lugar">Bogotá</div>
+                        <div className="fecha">Actualmente</div>
+                        <div className="lugar">-</div>
                     </div>
                     <div className="TituloInstitucion">
-                        <div className="Titulo">Psicología</div>
-                        <div className="Institucion">Institución Universitaria Politécnico Grancolombiano</div>
+                        <div className="Titulo">Diplomado en Pruebas Psicológicas</div>
+                        <div className="Institucion">Corporación Universitaria de Cataluña</div>
                     </div>
                     <div className="btnDescarga">
                         <a href="#">
