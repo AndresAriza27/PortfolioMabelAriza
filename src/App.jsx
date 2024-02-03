0fa933a915bd06314c6658a642bfd4885d28a1d8
Julian Andres Ariza <arjulian003@gmail.com>
2024-02-03 10:43:32 -0500

    Footer add

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
       <About />
       <Studies />
       <Jobs />
-      <Contact />
       <Footer />
     </div>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
+import '../styles/Footer.css'
+
+import icoPhone from '../imgs/iconPhone.png'
+import icoMail from '../imgs/iconMail.png'
+
 export function Footer () {
     return (
         <div className="footer">
-            <h3>Footer</h3>
+            <div className="contact">
+                <img src={icoPhone} alt="" />
+                <span>+57 3124011701</span>
+            </div>
+            <div className="contact">
+                <img src={icoMail} alt="" />
+                <span>maariza1701@gmail.com</span>
+            </div>
         </div>
     )
 }
