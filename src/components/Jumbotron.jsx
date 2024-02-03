eef1d16665386137fb0f034d4220292a19f840a0
Julian Andres Ariza <arjulian003@gmail.com>
2024-02-02 22:31:19 -0500

    Jumbotron styled

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,13 +1,15 @@
 import '../styles/Jumbotron.css'
+import imgJumbo from '../imgs/jumboImg.png'
 
 export function Jumbotron () {
     return (
         <div className="bkJtron">
             <div className="Jumbotron">
-            <div className="JmboTxt">
-                <h3>Mabel Zoraida Ariza Perez</h3>
-                <span>Psicologa</span>
-            </div>
+                <img src={imgJumbo} alt="" />
+                <div className="JmboTxt">
+                    <h3>Mabel Zoraida Ariza Perez</h3>
+                    <span>Psicologa</span>
+                </div>
             </div>
         </div>
     )
